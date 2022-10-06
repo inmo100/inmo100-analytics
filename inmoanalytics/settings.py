5f981d18fd5b602772631f51cfb6bd4f09ec275a
DanielCu <danielcu@alternet.com.mx>
2022-10-06 14:15:21 -0500

    secret

--- inmoanalytics/settings.py
+++ inmoanalytics/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key()) | 'django-insecure-%f1v34t0ncc=%9vodsq$mm96=jzdm*9!a+j@#)m(=60g4-eoao'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
