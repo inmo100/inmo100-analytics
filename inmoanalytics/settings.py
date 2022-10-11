e40b0b217520dba4b2f1c3dd6edf5ff1b1aeee0d
maneroto <maneroto@hotmail.com>
2022-10-11 11:18:34 -0500

    Remove unused imports in settings

--- inmoanalytics/settings.py
+++ inmoanalytics/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 from pathlib import Path
 import os
-import sys
-import dj_database_url
 import environ
-
-from django.core.management.utils import get_random_secret_key
 
 # Initialize the environment variables
 env = environ.Env()
