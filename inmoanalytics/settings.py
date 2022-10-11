d52ee5a9362317fba380f76115b0b52a5a3b4f7d
DanielCu <danielcu@alternet.com.mx>
2022-10-11 12:32:37 -0500

    changes setting py

--- inmoanalytics/settings.py
+++ inmoanalytics/settings.py
@@ -125,14 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = "/static/"
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
-<<<<<<< HEAD
-STATICFILES_DIRS = str(BASE_DIR / "static"),
-=======
-STATICFILES_DIRS = [str(BASE_DIR / "static"),]
->>>>>>> develop
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
