831f3bf396421aaa8e7e282ff897c7190288256a
Victor Avila <victoravila@MacBook-Pro-de-Victor.local>
2022-10-13 13:29:28 -0500

    Agregando cosas del admin

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,11 @@
+from atexit import register
+import site
 from django.contrib import admin
+from location.models import *
 
 # Register your models here.
+
+admin.site.register(State)
+admin.site.register(Zone)
+admin.site.register(Municipality)
+admin.site.register(Colony)
