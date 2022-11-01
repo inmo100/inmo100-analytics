7b3e33c599c3b6dcc8778e02076005114609630d
Victor Avila <victoravila@MacBook-Pro-de-Victor-2.local>
2022-11-01 14:54:25 -0600

    Adding equipments functionality

--- prototype/views.py
+++ prototype/views.py
@@ -18,10 +18,12 @@ import os.path
 def delete_prototypes(project_fields):
     project_prototypes = Prototype.objects.filter(project_field = project_fields)
     project_prototypes.delete()
+    
 #Function that recieves a list with all the rows from csv and the project field in order to register all the prototypes related
 # to a projet
 def save_data_csv(arr,project_field):
-    equipments = Equipment.objects.all()
+    equipments = Equipment.objects.all().order_by('id')
+    iterable2 = 10
     iterable = 16
     helper = []
     helper2 = []
@@ -43,72 +45,21 @@ def save_data_csv(arr,project_field):
             else:
                 property_type = PropertyType.objects.get(name=i[9])
          
-        if(i[10] == 'null'):
-            finishing = Finishing.objects.get(name='No existe')
-            helper.append(finishing)
-        else:
-            if(Finishing.objects.filter(name=i[10]).exists() == False):
+        for k in range(6):
+            if(i[iterable2] == 'null'):
                 finishing = Finishing.objects.get(name='No existe')
                 helper.append(finishing)
+                iterable2 = iterable2+1
             else:
-                finishing = Finishing.objects.get(name=i[10])
-                helper.append(finishing)
-        if(i[11] == 'null'):
-            finishing = Finishing.objects.get(name='No existe')
-            helper.append(finishing)
-        else:
-            if(Finishing.objects.filter(name=i[11]).exists() == False):
-                finishing = Finishing.objects.get(name='No existe')
-                helper.append(finishing)
-            else:
-                finishing = Finishing.objects.get(name=i[11])
-                helper.append(finishing)
-        if(i[12] == 'null'):
-            finishing = Finishing.objects.get(name='No existe')
-            helper.append(finishing)
-        else:
-            if(Finishing.objects.filter(name=i[12]).exists() == False):
-                finishing = Finishing.objects.get(name='No existe')
-                helper.append(finishing)
-            else:
-                finishing = Finishing.objects.get(name=i[12])
-                helper.append(finishing)
-        if(i[13] == 'null'):
-            finishing = Finishing.objects.get(name='No existe')
-            helper.append(finishing)
-        else:
-            if(Finishing.objects.filter(name=i[13]).exists() == False):
-                finishing = Finishing.objects.get(name='No existe')
-                helper.append(finishing)
-            else:
-                finishing = Finishing.objects.get(name=i[13])
-                helper.append(finishing)
-        if(i[14] == 'null'):
-            finishing = Finishing.objects.get(name='No existe')
-            helper.append(finishing)
-        else:
-            if(Finishing.objects.filter(name=i[14]).exists() == False):
-                finishing = Finishing.objects.get(name='No existe')
-                helper.append(finishing)
-            else:
-                finishing = Finishing.objects.get(name=i[14])
-                helper.append(finishing)
-        if(i[15] == 'null'):
-            finishing = Finishing.objects.get(name='No existe')
-            helper.append(finishing)
-        else:
-            if(Finishing.objects.filter(name=i[15]).exists() == False):
-                finishing = Finishing.objects.get(name='No existe')
-                helper.append(finishing)
-            else:
-                finishing = Finishing.objects.get(name=i[15])
-                helper.append(finishing)
-        cuantity = Equipment.objects.count()
-        for j in range(cuantity+1):
-            if(i[iterable] == 'null' or i[iterable] == 0):
-                iterable = iterable + 1
-            else:
-                helper2.append(i[iterable])
+                if(Finishing.objects.filter(name=i[iterable2]).exists() == False):
+                    finishing = Finishing.objects.get(name='No existe')
+                    helper.append(finishing)
+                    iterable2 = iterable2+1
+                else:
+                    finishing = Finishing.objects.get(name=i[iterable2])
+                    helper.append(finishing)
+                    iterable2 = iterable2+1
+        iterable2 = 10
         prototype = Prototype()
         prototype.segment_field = segment
         prototype.project_field = project
@@ -124,6 +75,24 @@ def save_data_csv(arr,project_field):
         prototype = Prototype.objects.get(name=i[0],project_field = project_field)
         prototype.finishings.set(helper)
         helper.clear()
+        iterable = 16
+        for equipment in equipments:
+            if(i[iterable] == 'null' or i[iterable] == 0):
+                equipment_quantity = EquipmentQuantity()
+                equipment_quantity.equipment = equipment
+                equipment_quantity.prototype = prototype
+                equipment_quantity.quantity = 0
+                equipment_quantity.save()
+                iterable = iterable + 1
+            else:
+                equipment_quantity = EquipmentQuantity()
+                equipment_quantity.equipment = equipment
+                equipment_quantity.prototype = prototype
+                equipment_quantity.quantity = i[iterable]
+                equipment_quantity.save()
+                iterable = iterable+1
+        iterable = 16
+
 #Function that saves and read the csv.
 def handle_uploaded_file(f,project_field,action):  
     with open('static/'+f.name, 'wb+') as destination:  
@@ -165,7 +134,7 @@ class PrototypesListView(ListView):
     template_name = 'pages/prototypes.html'
     model = Prototype
     def get(self, request, *args,**kwargs):
-        finishings_type = FinishingType.objects.exclude(name="No existe")
+        finishings_type = FinishingType.objects.exclude(name="No existe").order_by("id")
         return render(request,self.template_name,context={
             'prototype_list': Prototype.objects.all(),
             'finishings_type': finishings_type
@@ -195,7 +164,7 @@ class UpdatePrototype(ListView):
 def download_csv():
     if(os.path.isfile("static/plantilla_prototipos2.csv")):
         remove("static/plantilla_prototipos2.csv")
-    equipments = Equipment.objects.all()
+    equipments = Equipment.objects.all().order_by('id')
     template = pd.read_csv("static/plantilla_prototipos.csv")
     arr = [""]
     for equipment in equipments:
