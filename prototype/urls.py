bd6b6994c24061081c288382ce14b40e20f3ed0c
maneroto <maneroto@hotmail.com>
2022-11-09 16:05:47 -0600

    Create base filters

--- prototype/urls.py
+++ prototype/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.PrototypesListView.as_view(), name='prototypes'),
     path('update/<id>', views.UpdatePrototype.as_view(), name='update_prototype'),
     path('download_format/', views.download_csv, name='download_format'),
-
 ]
