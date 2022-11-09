5722b0097efd20180098c630466b5aa188dbceb7
A01704868 <A01704868@itesm.mx>
2022-11-09 13:51:14 -0600

    no se descarga bien la plantilla de csv

--- prototype/urls.py
+++ prototype/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', views.PrototypesListView.as_view(), name='prototypes'),
     path('update/<id>', views.UpdatePrototype.as_view(), name='update_prototype'),
     path('download_format/', views.download_csv, name='download_format'),
-    path('filters', filter_view, name="filter"),
 
 ]
