24589f59ef01c1be3bb1d5c1139a0066177f62ee
Victor Avila <victoravila@Mac-mini-de-Victor.local>
2022-11-15 12:29:23 -0600

    Declaring managers

--- prototype/managers.py
+++ prototype/managers.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.db.models import Count, Q
-from django.db.models.functions import Concat
 from .models import *
-from pprint import pprint
 import numpy
 
 class PrototypesManager(models.Manager):
