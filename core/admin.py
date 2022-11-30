cf33b00c96439b9e5ea60ece465499622d3f5fa9
A01704868 <A01704868@itesm.mx>
2022-11-30 14:46:35 -0600

    easier navigation link

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 
 admin.site.site_header = 'Administración IMNO100'                    
 admin.site.index_title = 'Base de Datos'
+admin.site.site_url = '/proyectos/desarrolladoras'
